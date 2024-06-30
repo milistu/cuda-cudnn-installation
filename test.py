e0585c14b7577a2ed69044a52c6c43a44f698a8e
Milutin Studen <81291567+milistu@users.noreply.github.com>
2024-06-30 23:07:39 +0200

    Update test.py
    Add variable device checkup

--- test.py
+++ test.py
@@ -6,3 +6,12 @@ print(f"PyTorch version: {torch.__version__}")
 # Check if CUDA device is recognised
 print(f"CUDA device is available: {torch.cuda.is_available()}")
 assert torch.cuda.is_available(), ValueError("CUDA device is not recognised.")
+
+device = "cuda"
+a = torch.rand(3, 1).to(device)
+b = torch.rand(1, 3).to(device)
+
+c = a @ b
+
+print(f"Variable shape: {c.shape}")
+print(f"Variable devie: {c.device}")
